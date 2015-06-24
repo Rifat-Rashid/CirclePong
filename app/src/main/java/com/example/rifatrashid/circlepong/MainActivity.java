df1b99ad582203ab5ab26fb312799d9154d5ef54
Rifat-Rashid <s-RRashid@lwsd.org>
2015-06-24 02:31:24 -0700

    Updates: -MainActivity Screen -> Increased button size, applied Lato font, white button outline -MainActivity.Java -> Applied lato font to header title

--- app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.rifatrashid.circlepong;
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private TextView titleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Typeface lato = Typeface.createFromAsset(getAssets(), "fonts/Lato-Thin.ttf");
+        titleText = (TextView) findViewById(R.id.titleText);
+        titleText.setTypeface(lato);
     }
 }
