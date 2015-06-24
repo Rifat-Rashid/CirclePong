8a5754535fd5f5869b223dd397c00eeacc88bf87
Rifat-Rashid <s-RRashid@lwsd.org>
2015-06-24 05:05:55 -0700

    Modifications Applied!

--- app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.rifatrashid.circlepong;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
     private TextView titleText;
+    private Button singleplayer, leaderboards, settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,32 @@ public class MainActivity extends Activity {
         Typeface lato = Typeface.createFromAsset(getAssets(), "fonts/Lato-Thin.ttf");
         titleText = (TextView) findViewById(R.id.titleText);
         titleText.setTypeface(lato);
+        singleplayer = (Button) findViewById(R.id.singleplayer_btn);
+        leaderboards = (Button) findViewById(R.id.leaderboards_btn);
+        settings = (Button) findViewById(R.id.settings_btn);
+
+        //when singleplayer button is clicked...
+        singleplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //when leaderboards button is clicked...
+        leaderboards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //when settings button is clicked...
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
