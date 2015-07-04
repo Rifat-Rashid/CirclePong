f3f404d533887cc8b9944dda9048a674ee45a4c8
Rifat-Rashid <s-RRashid@lwsd.org>
2015-07-04 04:10:23 -0700

    Changes made...

--- app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.rifatrashid.circlepong;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
     private TextView titleText;
     private arena Arena;
+    private Handler handlerApplication;
     private Ball ball;
     private paddle Paddle;
     private ImageView imageView;
@@ -26,37 +28,44 @@ public class MainActivity extends Activity {
     private Paint ballPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final int ADDITIONAL_WIDTH = 10;
     private final int ADDITIONAL_PADDLEARC = 25;
+     private SurfaceHolder _surfaceHolder;
+    private GameThread thread;
+    private SurfaceView _surfaceView;
     private Bitmap bitmap, tempBitmap;
+    private arena Arenaa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        _surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        _surfaceHolder = _surfaceView.getHolder();
+        _surfaceHolder.addCallback(this);
+
         imageView = (ImageView) findViewById(R.id.imageView);
-        setupCanvas();
+        //setupCanvas();
+        /*
         //Create custom bitmap to draw on!
         new Thread(new Runnable() {
             @Override
             public void run() {
                 //Draw to bitmap!
-                for(int i = 0; i <= 360; i++) {
-                    Bitmap tempBitmap = Bitmap.createBitmap(120, 120, Bitmap.Config.RGB_565);
-                    Canvas canvas = new Canvas(tempBitmap);
-                    canvas.drawColor(Color.parseColor("#191919"));
-                    Arena.Draw(canvas);
-                    ball.Draw(canvas);
-                    Paddle.Draw(canvas);
-                    imageView.setImageBitmap(tempBitmap);
-                    Paddle.setArcLength(i);
-                }
+                Bitmap tempBitmap = Bitmap.createBitmap(120, 120, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(tempBitmap);
+                canvas.drawColor(Color.parseColor("#191919"));
+                Arena.Draw(canvas);
+                ball.Draw(canvas);
+                Paddle.Draw(canvas);
+                imageView.setImageBitmap(tempBitmap);
             }
         }).start();
+        */
         Typeface lato = Typeface.createFromAsset(getAssets(), "fonts/Lato-Thin.ttf");
         titleText = (TextView) findViewById(R.id.titleText);
         titleText.setTypeface(lato);
     }
 
-    private void setupCanvas(){
+    private void setupCanvas() {
         myPaint.setAntiAlias(true);
         myPaint.setStyle(Paint.Style.STROKE);
         myPaint.setStrokeWidth(2);
@@ -70,5 +79,99 @@ public class MainActivity extends Activity {
         ball.setPaint(ballPaint);
         Paddle = new paddle(Arena.getX() - Arena.getRadius(), Arena.getY() - Arena.getRadius(), Arena.getRadius() * 2 + ADDITIONAL_WIDTH, Arena.getRadius() * 2 + ADDITIONAL_WIDTH, ADDITIONAL_PADDLEARC, 30);
         Paddle.setStroke(3);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread = new GameThread(_surfaceHolder, new Handler() {
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void publish(LogRecord record) {
+
+            }
+        });
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        thread.setSurfaceSize(width, height);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        thread.setRunning(false);
+        while(retry){
+            try{
+                thread.join();
+                retry = false;
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class  GameThread extends Thread{
+        private int canvasWidth;
+        private int canvasHeight;
+        private boolean run = false;
+
+        public GameThread(SurfaceHolder surfaceHolder,Handler handler){
+            _surfaceHolder = surfaceHolder;
+            handlerApplication = handler;
+        }
+
+        public void doStart(){
+            synchronized (_surfaceHolder){
+                //Initialize stuff here!
+                Arenaa = new arena(300, 300, 50);
+            }
+        }
+
+        public void run(){
+            while(run){
+                Canvas c = null;
+                try{
+                    c = _surfaceHolder.lockCanvas(null);
+                    synchronized (_surfaceHolder){
+                        doDraw(c);
+                    }
+                }finally {
+                    if(c != null){
+                        _surfaceHolder.unlockCanvasAndPost(c);
+                    }
+                }
+            }
+        }
+
+        public void setRunning(boolean b){
+            run = b;
+        }
+
+        public void setSurfaceSize(int width, int height){
+            synchronized (_surfaceHolder){
+                canvasHeight = height;
+                canvasWidth = width;
+                doStart();
+            }
+        }
+
+        private void doDraw(final Canvas canvas){
+            canvas.save();
+            canvas.drawColor(Color.parseColor("#1abc9c"));
+            Arenaa.Draw(canvas);
+            canvas.restore();
+        }
     }
 }
