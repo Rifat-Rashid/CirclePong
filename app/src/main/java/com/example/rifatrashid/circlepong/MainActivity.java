c208d64ee407a472ed3959386fa723d2c87a33d3
Rifat-Rashid <s-RRashid@lwsd.org>
2015-07-05 19:09:33 -0700

    works...

--- app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
+++ app/src/main/java/com/example/rifatrashid/circlepong/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.drawable.PaintDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.ImageView;
@@ -26,6 +27,8 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
     private GameThread thread;
     private SurfaceView _surfaceView;
     private paddle Paddle;
+    private final int DEGREE_ARC_1 = 270;
+    private int arc1Length = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +37,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         _surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         _surfaceHolder = _surfaceView.getHolder();
         _surfaceHolder.addCallback(this);
-        //setupCanvas();
-        /*
-        //Create custom bitmap to draw on!
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //Draw to bitmap!
-                Bitmap tempBitmap = Bitmap.createBitmap(120, 120, Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(tempBitmap);
-                canvas.drawColor(Color.parseColor("#191919"));
-                Arena.Draw(canvas);s
-                ball.Draw(canvas);
-                Paddle.Draw(canvas);
-                imageView.setImageBitmap(tempBitmap);
-            }
-        }).start();
-        */
         Typeface lato = Typeface.createFromAsset(getAssets(), "fonts/Lato-Thin.ttf");
         titleText = (TextView) findViewById(R.id.titleText);
         titleText.setTypeface(lato);
@@ -74,8 +60,19 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
             }
         });
-        thread.setRunning(true);
-        thread.start();
+        new CountDownTimer(1500, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //Do Nothing!
+            }
+
+            @Override
+            public void onFinish() {
+                thread.setRunning(true);
+                thread.start();
+            }
+        }.start();
     }
 
     @Override
@@ -87,73 +84,75 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
     public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
         thread.setRunning(false);
-        while(retry){
-            try{
+        while (retry) {
+            try {
                 thread.join();
                 retry = false;
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    class  GameThread extends Thread{
+    class GameThread extends Thread {
         private int canvasWidth;
         private int canvasHeight;
         private boolean run = false;
 
-        public GameThread(SurfaceHolder surfaceHolder,Handler handler){
+        public GameThread(SurfaceHolder surfaceHolder, Handler handler) {
             _surfaceHolder = surfaceHolder;
             handlerApplication = handler;
         }
 
-        public void doStart(){
-            synchronized (_surfaceHolder){
+        public void doStart() {
+            synchronized (_surfaceHolder) {
                 //Initialize stuff here!
-                Paddle = new paddle(15,15 ,585, 585, 0, 360);
+                Paddle = new paddle(15, 15, 585, 585, DEGREE_ARC_1, arc1Length);
                 Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
                 paint.setAntiAlias(true);
                 paint.setStyle(Paint.Style.STROKE);
-                paint.setStrokeWidth(2);
+                paint.setStrokeWidth(4);
                 paint.setColor(Color.parseColor("#FF2D55"));
-                myPaint.setColor(Color.parseColor("#FFFFFF"));
                 Paddle.setPaint(paint);
             }
         }
 
-        public void run(){
-            while(run){
+        public void run() {
+            while (run) {
                 Canvas c = null;
-                try{
+                try {
                     c = _surfaceHolder.lockCanvas(null);
-                    synchronized (_surfaceHolder){
+                    synchronized (_surfaceHolder) {
                         doDraw(c);
                     }
-                }finally {
-                    if(c != null){
+                } finally {
+                    if (c != null) {
                         _surfaceHolder.unlockCanvasAndPost(c);
                     }
                 }
             }
         }
 
-        public void setRunning(boolean b){
+        public void setRunning(boolean b) {
             run = b;
         }
 
-        public void setSurfaceSize(int width, int height){
-            synchronized (_surfaceHolder){
+        public void setSurfaceSize(int width, int height) {
+            synchronized (_surfaceHolder) {
                 canvasHeight = height;
                 canvasWidth = width;
                 doStart();
             }
         }
 
-        private void doDraw(final Canvas canvas){
+        private void doDraw(final Canvas canvas) {
             canvas.save();
             canvas.drawColor(Color.parseColor("#191919"));
-            //Arenaa.Draw(canvas);
             Paddle.Draw(canvas);
+            if (arc1Length <= 360) {
+                arc1Length += 2;
+                Paddle.setArcLength(arc1Length);
+            }
             canvas.restore();
         }
     }
